010f09928c652cbb71f6bc1129e3502f843ede54
Andrea Zonca <zonca@login01.expanse.sdsc.edu>
2021-07-26 04:38:59 -0700

    use variable for image

--- 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
+++ 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
@@ -1,3 +1,3 @@
 export PATH="/cm/shared/apps/sdsc/galyleo:${PATH}"
-galyleo.sh launch --account 'sds166' --partition 'shared' --cpus-per-task 16 --time-limit 00:30:00 --jupyter 'lab' --notebook-dir "${HOME}"  --sif '/expanse/lustre/projects/sds166/zonca/dask-numba-si21.sif'
-
+SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si21.sif'
+galyleo.sh launch --account 'sds166' --partition 'shared' --cpus-per-task 16 --time-limit 00:30:00 --jupyter 'lab' --notebook-dir "${HOME}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
