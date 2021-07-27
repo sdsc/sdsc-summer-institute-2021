0ec4350c0c6d5bdca3347a66b36ca7a6a29442be
Andrea Zonca <zonca@login01.expanse.sdsc.edu>
2021-07-27 02:07:53 -0700

    set Summer institute account

--- 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
+++ 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
@@ -1,3 +1,6 @@
 export PATH="/cm/shared/apps/sdsc/galyleo:${PATH}"
 SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si21.sif'
-galyleo.sh launch --account 'sds166' --partition 'shared' --cpus-per-task 16 --time-limit 00:30:00 --jupyter 'lab' --notebook-dir "${HOME}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
+# Summer institute account
+ACCOUNT=crl155
+# ACCOUNT=sds166
+galyleo.sh launch --account $ACCOUNT --partition 'compute' --cpus-per-task 64 --time-limit 01:00:00 --jupyter 'lab' --notebook-dir "${HOME}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
