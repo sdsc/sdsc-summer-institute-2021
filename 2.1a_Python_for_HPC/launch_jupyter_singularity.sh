652a728f50da5a7ee5f46b1e5f1f5a08aba27e3f
Andrea Zonca <zonca@login01.expanse.sdsc.edu>
2021-07-28 08:39:57 -0700

    use current folder for jupyterlab

--- 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
+++ 2.1a_Python_for_HPC/launch_jupyter_singularity.sh
@@ -3,4 +3,8 @@ SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si21.sif'
 # Summer institute account
 ACCOUNT=crl155
 # ACCOUNT=sds166
-galyleo.sh launch --account $ACCOUNT --partition 'compute' --cpus-per-task 64 --time-limit 01:00:00 --jupyter 'lab' --notebook-dir "${HOME}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
+# Use current folder
+NOTEBOOK_FOLDER=$(pwd -P)
+# Use the user home
+# NOTEBOOK_FOLDER=${HOME}
+galyleo.sh launch --account $ACCOUNT --partition 'compute' --cpus-per-task 64 --time-limit 01:00:00 --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
